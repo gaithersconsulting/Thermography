c7c379fc0db01ccbd8301aba3af850817f91bb92
Erick Gaither <erick.gaither@gmail.com>
2024-08-12 11:22:53 -0400

    Initial Site

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Set the year in the footer
+    let year = new Date().getFullYear();
+    document.getElementById("year").innerHTML = year;
+});
